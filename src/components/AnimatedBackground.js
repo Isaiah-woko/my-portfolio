4a7d81950ced85ac12ae3f191efaf0e10fd489fc
Isaiah woko <wokoisaiah68@gmail.com>
2025-02-20 16:07:42 +0100

    fixed the lag

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -36,37 +36,7 @@ const AnimatedBackground = () => {
         }}
       />
 
-      {/* Subtle particle effect */}
-      <motion.div
-        className="absolute inset-0"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 0.5 }}
-        transition={{ duration: 2, delay: 1 }}
-      >
-        {Array.from({ length: 30 }).map((_, i) => (
-          <motion.div
-            key={i}
-            className="absolute w-1 h-1 bg-white rounded-full"
-            initial={{
-              x: Math.random() * window.innerWidth,
-              y: Math.random() * window.innerHeight,
-              scale: 0,
-            }}
-            animate={{
-              x: Math.random() * window.innerWidth,
-              y: Math.random() * window.innerHeight,
-              scale: [0, 1, 0],
-              opacity: [0, 1, 0],
-            }}
-            transition={{
-              duration: 5,
-              repeat: Infinity,
-              ease: "easeInOut",
-              delay: Math.random() * 2,
-            }}
-          />
-        ))}
-      </motion.div>
+      
     </motion.div>
   );
 };
